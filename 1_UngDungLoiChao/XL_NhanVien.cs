42ff006ccfdc9076f9fb8ffc33a1e6c699177917
Joy Trinh <mongtuyen149@gmail.com>
2019-01-06 22:31:42 +0700

    Complete the project with 4 solutions

---
+++ 1_UngDungLoiChao/XL_NhanVien.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+    public class XL_NhanVien
+    {
+        public string HoTen, MaSo;
+        public int SoLanChao;
+    }
---
+++ 1_UngDungLoiChao/XL_QuanLy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+    public class XL_QuanLy
+    {
+        public string HoTen, MaSo;
+        public int SoLanChao;
+    }
